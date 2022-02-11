2ea4be290982a03cb42be676be455b2d6530db66
Taylor Johnson <taylor.johnson.veteran@outlook.com>
2022-02-11 08:01:53 -0800

    added 1-14-22 R file

---
+++ 1-14-22.R
@@ -0,0 +1,5 @@
+# This code is from: https://www.statmethods.net/graphs/boxplot.html
+# Boxplot of MPG by Car Cylinders
+boxplot(mpg~cyl,data=mtcars, main="Car Milage Data",
+        xlab="Number of Cylinders", ylab="Miles Per Gallon") 
+
