0bda2ee628bec797654aa55faea7f332312932ff
Taylor Johnson <taylor.johnson.veteran@outlook.com>
2022-02-15 13:07:31 -0800

    deleted this R file

--- 1-14-22.R
+++
@@ -1,5 +0,0 @@
-# This code is from: https://www.statmethods.net/graphs/boxplot.html
-# Boxplot of MPG by Car Cylinders
-boxplot(mpg~cyl,data=mtcars, main="Car Milage Data",
-        xlab="Number of Cylinders", ylab="Miles Per Gallon") 
-
