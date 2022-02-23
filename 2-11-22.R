d1eab2683fda34186e5ffdc77933c1bc0a4a4e07
Taylor Johnson <taylor.johnson.veteran@outlook.com>
2022-02-23 13:21:54 -0800

    updated code throughout

--- 2-11-22.R
+++ 2-11-22.R
@@ -1,50 +1,57 @@
 # Slide deck: Exercises on ggplot2 (3)
 
 library(tidyverse)
+library(ggplot2)
 load("county_clean.Rdata")
 state_region <- read_csv("state_region.csv")
 
-# left_join code from end of 2-7-22
-# Use left_join to join the region dataframe to the bulk of the data. Glimpse the result.
-# LOOK AT HIS CODE
-county_clean_joined = county_clean %>%
-  left_join(state_region, by = c("state" = "State"))
-glimpse(county_clean_joined)
-
+# Use left_join to add the region data to county_clean. Glimpse county_clean to make sure you’re OK.
+county_clean = county_clean %>%
+  left_join(state_region, by = c("state" = "State")) %>%
+  glimpse()
 
 # Display the distribution of per_capita_income using a histogram.
-county_clean_joined %>%
-  ggplot(aes(per_capita_income)) + 
+county_clean %>%
+  ggplot(aes(x=per_capita_income)) + 
   geom_histogram()
 
-# see slide 6
+# The data displayed by default with a histogram is the count of observations in each bin. You can also ask that the data be normalized so that the total area of the histogram is equal to 1.0. 
+# To see this, in the call to geom_histogram() insert aes(y = ..density..).
+# The .. .. indicates to ggplot2 that this “variable” is not present in the dataframe, but will be made available.
+county_clean %>% 
+  ggplot(aes(x = per_capita_income)) +
+  geom_histogram(aes(y = ..density..))
 
-# see slide 7
-# Display the distribution per_capita_income using geom_density()
-county_clean_joined %>%
+# We have an alternative, geom_density(), to geom_histogram() which displays the distribution of a single quantitative variable.
+# Display the distribution of per_capita_income using geom_density() in place of geom_histogram().
+county_clean %>% 
+  ggplot(aes(x = per_capita_income)) + 
+  geom_density()  # makes a curved continuous line instead of jagged bars
+
+# Add geom_histogram() to the previous graph for comparison.
+county_clean %>% 
+  ggplot(aes(x = per_capita_income)) + 
+  geom_histogram(aes(y = ..density..)) +
+  geom_density() 
+
+# geom_density has an optional parameter, adjust. The default value is 1. Try setting it to .5 and 2.
+county_clean %>%
   ggplot(aes(per_capita_income)) + 
-  geom_density()
+  geom_density(adjust = 0.5)  # shows more shape detail along the line
 
-# see slide 9
-
-
-# see slide 10
-county_clean_joined %>%
+county_clean %>%
   ggplot(aes(per_capita_income)) + 
-  geom_density(adjust = 0.5)  # shows more shape detail on line
+  geom_density(adjust = 2)  # shows less shape detail along the line, more of a curve; equivalent to using smaller number of bins in a histogram
 
-county_clean_joined %>%
-  ggplot(aes(per_capita_income)) + 
-  geom_density(adjust = 2)  # shows less shape detail on line/more of a curve; equivalent to using smaller number of bins in a histogram
-
-# see slide 12
-county_clean_joined %>%
+# Use geom_density() and add geom_rug(). There is a stroke for each observation.
+county_clean %>%
   ggplot(aes(per_capita_income)) + 
   geom_density() +
   geom_rug()  # one stroke per observation is displayed along the x-axis
 
-# see slide 15
 # Do a facet by region of the last graph
-
-
-
+county_clean %>% 
+  ggplot(aes(x = per_capita_income)) + 
+  geom_density() +
+  geom_rug() +
+  facet_wrap(~Region, ncol = 1)  # creates one graph for each region (so four graphs total)
