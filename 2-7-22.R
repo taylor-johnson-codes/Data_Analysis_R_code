1c941363c1ab68cf92132133953a678a53f82afd
Taylor Johnson <taylor.johnson.veteran@outlook.com>
2022-02-23 12:50:13 -0800

    updated code in the first half and added code to the second half of the file

--- 2-7-22.R
+++ 2-7-22.R
@@ -1,6 +1,7 @@
 # Slide deck: Exercises on ggplot2
 
 library(tidyverse)
+library(ggplot2)
 load("county_clean.Rdata")
 state_region <- read_csv("state_region.csv")
 
@@ -9,7 +10,7 @@ glimpse(state_region)  # note column State is capitalized
 
 # Do a simple scatter plot of per_capita_income on the y-axis against homeownership on the x-axis.
 county_clean %>%
-  ggplot(aes(homeownership, per_capita_income)) +
+  ggplot(aes(x=homeownership, y=per_capita_income)) +
   geom_point()
 
 # alternative:
@@ -26,15 +27,16 @@ county_clean %>%
 # on the x-axis and the income variable on the y-axis. Use geom_point().
 county_clean %>%
   ggplot(aes(median_edu, per_capita_income)) +
-  geom_point()  # plot has a lot of overlapping
+  geom_point()  # plot has a lot of overlapping/overplotting
 
 # Repeat with geom_jitter(). Play with the size parameter to get a value you like.
 county_clean %>%
   ggplot(aes(median_edu, per_capita_income)) +
   geom_jitter(size = 0.5)  # can see how many data points there are for each x-axis value since they're no longer overlapping
 
+# Make the graph look more professional.
 # Add labels to the graph
-# LOOK AT HIS CODE - SLIDE 15
+# my attempt:
 county_clean %>%
   ggplot(aes(median_edu, per_capita_income)) +
   geom_jitter(size = 0.5) +
@@ -42,26 +44,35 @@ county_clean %>%
   ylab("Income Level") +
   ggtitle("Per Capita Income by Education Level", subtitle = "US Counties 2017")
 
+# his nicer looking code:
+county_clean %>% 
+  ggplot(aes(x = median_edu, y = per_capita_income)) +
+  geom_jitter(size = .5) +
+  labs(x = "Median Educational Level",
+       y = "Per Capita Income",
+       title = "Per Capita Income by Education Level",
+       subtitle = "US Counties 2017")
+
 # Use left_join to join the region dataframe to the bulk of the data. Glimpse the result.
-# LOOK AT HIS CODE
-county_clean_joined = county_clean %>%
-  left_join(state_region, by = c("state" = "State"))
+county_clean = county_clean %>% 
+  left_join(state_region,by = c("state" = "State")) %>%
+  glimpse()
 
-glimpse(county_clean_joined)
+# Use geom_bar and fill to describe the relationship between Regian and median_edu. Make Region a factor first. In the call to geom_bar() set color = “white”.
+county_clean = county_clean %>% 
+  mutate(Region = factor(Region))
 
-# missed slide 18-19
+county_clean %>% 
+  ggplot(aes(x = Region, fill = median_edu)) +
+  geom_bar(color = "white")
 
+# The default value of the parameter position is “stack”. Try setting position = “dodge” and “dodge2”.
+county_clean %>% 
+  ggplot(aes(x = Region, fill = median_edu)) +
+  geom_bar(color = "white",position = "dodge") +
+  ggtitle("position = 'dodge'")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+county_clean %>% 
+  ggplot(aes(x = Region, fill = median_edu)) +
+  geom_bar(color = "white",position = "dodge2") +
+  ggtitle("position = 'dodge2'")
