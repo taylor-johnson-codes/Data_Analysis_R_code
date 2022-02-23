cb800face36324b4b8bf64fe0f6f0d8c4971b845
Taylor Johnson <taylor.johnson.veteran@outlook.com>
2022-02-23 13:05:57 -0800

    updated code throughout

--- 2-9-22.R
+++ 2-9-22.R
@@ -1,25 +1,26 @@
 # Slide deck: Exercises on ggplot2 (2)
 
 library(tidyverse)
+library(ggplot2)
+library(dplyr)
 load("county_clean.Rdata")
 state_region <- read_csv("state_region.csv")
 
-# left_join code from end of 2-7-22
-# Use left_join to join the region dataframe to the bulk of the data. Glimpse the result.
-# LOOK AT HIS CODE
-county_clean_joined = county_clean %>%
-  left_join(state_region, by = c("state" = "State"))
-glimpse(county_clean_joined)
+# Use left_join to add the region data to county_clean. Glimpse county_clean to make sure you’re OK.
+county_clean = county_clean %>%
+  left_join(state_region, by = c("state" = "State")) %>%
+  glimpse()
 
 # An alternative for Two Categorical Variables
 # Examine the relationship between median educational level and region. Put region on the x-axis and median education on
 # the y-axis. Use geom_jitter(). Try some different size values in jitter.
-county_clean_joined %>%
-  ggplot(aes(Region, median_edu)) + 
+county_clean %>%
+  ggplot(aes(x=Region, y=median_edu)) + 
   geom_jitter(size = 0.8)
 
+# Another Alternative
 # Repeat with geom_count() instead
-county_clean_joined %>%
+county_clean %>%
   ggplot(aes(Region, median_edu)) + 
   geom_count()
 
@@ -27,22 +28,23 @@ county_clean_joined %>%
 # Cleveland style (starting simple and progressing to more complex)
 
 # Get a barplot of the number of counties in each state using geom_bar().
-county_clean_joined %>%
-  ggplot(aes(state)) +
+county_clean %>%
+  ggplot(aes(x=state)) +
   geom_bar()  # x-axis labels are unreadable due to overlapping
 
 # Add coord_flip() as a layer
-county_clean_joined %>%
+county_clean %>%
   ggplot(aes(state)) +
   geom_bar() +
   coord_flip()  # now they're on the y-axis and readable
 
-# Use dplyr to create a dataframe state_counties with state name and the count of counties
-state_counties <- county_clean_joined %>%
+# Use dplyr to create a dataframe state_counties with state name and the count of counties.
+state_counties <- county_clean %>%
   group_by(state) %>%
   summarize(count = n()) %>%
   ungroup()
-state_counties
+
+head(state_counties)
 
 # Use state_counties as the data argument of ggplot. In the aes, max x to count and y to state. Use geom_col()
 state_counties %>%
@@ -67,14 +69,11 @@ state_counties %>%
 
 
 # Get a histogram of pop2017
-county_clean_joined %>%
-  ggplot(aes(pop2017)) + 
-  geom_histogram()  # not useful without logarithmic scale
+county_clean %>%
+  ggplot(aes(x=pop2017)) + 
+  geom_histogram()  # not useful without logarithmic scale because of the extreme right skew
 
-county_clean_joined %>%
+county_clean  %>%
   ggplot(aes(pop2017)) + 
   geom_histogram() +
   scale_x_log10()
-
-
-
