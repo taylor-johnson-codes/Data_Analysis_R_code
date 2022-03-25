531684ecd808d7a777a1991d85f0f3bf3f45ab11
Taylor Johnson <taylor.johnson.veteran@outlook.com>
2022-03-24 17:25:50 -0700

    updated comments

--- 3-16-22.R
+++ 3-16-22.R
@@ -1,18 +1,14 @@
 # slide deck: HMD-Human Mortality Database 1
 # data from: https://www.mortality.org/
 # zipped data files on left, Life Tables for Male/Female/Both Sexes, register and get account to get the data file
-# gives you one txt file for each country, pick USA file
-# data goes back to 1933
+# gives you one txt file for each country, pick USA file (data goes back to 1933)
 
 library(tidyverse)
 library(plotly)
 
-# download and import/load dataset
-
 # Load the data for USA males. Add a variable country and set it to “USA”.
 # Select country, Year, Age and qx (probability of dying within the following year).
-# Make Age numeric.
-# Eliminate any missing data.
+# Make Age numeric. Eliminate any missing data.
 
 USA <- read_table("~/Dropbox/HMD/lt_male/mltper_1x1/USA.mltper_1x1.txt", skip = 2) %>% 
   mutate(country = "USA") %>% 
